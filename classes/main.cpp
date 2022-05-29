675dd6b829c52d6b1a1a67d29de14535936e41b0
hugoabreu1002 <hugoabreu1002@gmail.com>
2022-05-29 00:55:39 -0300

    first class

---
+++ classes/main.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+
+const double PI {3.1415926535897932384626433832795};
+
+class Cylinder {
+    public : 
+        //Constctors
+        Cylinder(){
+            base_radius = 1.0;
+            height = 1.0;
+        }
+
+        Cylinder(double rad_param,double height_param){
+            base_radius = rad_param;
+            height = height_param;
+        }
+   
+        //Functions (methods)
+        double volume(){
+            return PI * base_radius * base_radius * height;
+        }
+
+    private : 
+        //Member variables
+        double base_radius{};
+        double height{};
+};
+
+
+int main(){
+    //Cylinder cylinder1(10,4); // Object
+    Cylinder cylinder1;
+    std::cout << "volume : " << cylinder1.volume() << std::endl;
+
+    return 0;
+}
