3ab635eace9c367af8115b4feab2e044c52e6584
hugoabreu1002 <hugoabreu1002@gmail.com>
2022-06-08 22:28:03 -0400

    abstract factory reviewed and Pointer to objects

---
+++ DesignPatterns/CreationalPatterns/03_03_FactoryRun/03_03_FactoryRun_End.cpp
@@ -0,0 +1,81 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+// Product from which the concrete products will inherit from 
+class Coffee
+{
+protected:
+	char _type[15];
+public:
+	Coffee()
+	{
+	}
+	char *getType()
+	{
+		return _type;
+	}
+};
+
+// One concrete class
+class Espresso : public Coffee
+{
+public:
+	Espresso() : Coffee()
+	{
+		strcpy(_type, "Espresso");
+		cout << endl << "Making a cup of espresso" << endl;
+		cout << "Grind and brew one scoop of espresso beans." << endl;
+	}
+};
+
+// Another concrete class
+class Cappuccino : public Coffee
+{
+public:
+	Cappuccino() : Coffee()
+	{
+		strcpy(_type, "Cappuccino");
+		cout << endl << "Making a cup of cappuccino" << endl;
+		cout << "Grind and brew one scoop of espresso beans." << endl;
+		cout << "Heat and foam milk." << endl;
+	}
+};
+
+class CoffeeMakerFactory
+{
+private:
+	Coffee * _coffee;
+public:
+	Coffee * GetCoffee()
+	{
+		int choice;
+
+		cout << "Select type of coffee to make: " << endl;
+		cout << "1: Espresso" << endl;
+		cout << "2: Cappuccino" << endl;
+		cout << "Selection: ";
+		cin >> choice;
+
+		switch (choice)
+		{
+		case 1:
+			return new Espresso;
+		case 2:
+			return new Cappuccino;
+		default:
+			cout << "Invalid Selection" << endl;
+			return NULL;
+		}
+	}
+};
+
+int main()
+{
+	CoffeeMakerFactory coffeeMachine;
+	Coffee *cup;
+	cup = coffeeMachine.GetCoffee();
+
+	cout << endl << "You had asked for a(n)" << cup->getType() << endl;
+}
